9540dfa73bb55c02b828dd9dde3fe0cbd52e7120
may <parkjh080@naver.com>
2023-08-23 19:31:28 +0900

    feat(study): Save Gson data in UDDT List Object using I/0 Stream 23-08-23
    Today, Learn about Save Gson data in UDDT List Object using I/0 Stream
    (API04.java, BookList.java)

---
+++ src/main/java/org/example/model/BookList.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookList {
+    private List<Book> books;
+}
